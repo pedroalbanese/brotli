0766663f43e77e062963097d6151efada1fce187
Pedro Albanese <68971450+pedroalbanese@users.noreply.github.com>
2021-10-30 17:41:56 -0300

    Command-line Tool

--- cmd/brotli/main.go
+++ cmd/brotli/main.go
@@ -1,5 +1,5 @@
 // Copyright (c) 2021, Pedro Albanese. All rights reserved.
-// Use of this source code is governed by a ISC license that
+// Use of this source code is governed by a MIT license that
 // can be found in the LICENSE file.
 
 package main
